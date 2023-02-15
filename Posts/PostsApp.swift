b59d7b50a51ecca591d325e171671e93ee933010
Artem Bartle <ar.bartle@gmail.com>
2023-02-15 09:13:47 +0100

    Initial Commit

---
+++ Posts/PostsApp.swift
@@ -0,0 +1,12 @@
+// Developed by ___ORGANIZATIONNAME___
+
+import SwiftUI
+
+@main
+struct PostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
